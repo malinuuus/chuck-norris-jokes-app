505db4bc8b62d634e28718633d51ff1897350e4c
malinuuus <maliny.brzoskwiniowe@gmail.com>
2024-07-03 15:19:37 +0200

    Filter jokes by a category

--- server/src/jokes/jokes.controller.ts
+++ server/src/jokes/jokes.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { JokesService } from './jokes.service';
 import { JokeDto } from './dto/joke.dto';
+import { GetJokeFilterDto } from './dto/get-joke-filter.dto';
 
 @Controller('jokes')
 export class JokesController {
   constructor(private jokesService: JokesService) {}
 
   @Get('random')
-  getRandomJoke(): Promise<JokeDto> {
-    return this.jokesService.getRandomJoke();
+  getRandomJoke(@Query() filterDto: GetJokeFilterDto): Promise<JokeDto> {
+    return this.jokesService.getRandomJoke(filterDto);
   }
 }
--- server/src/jokes/jokes.service.ts
+++ server/src/jokes/jokes.service.ts
@@ -5,14 +5,22 @@ import { catchError, firstValueFrom } from 'rxjs';
 import { Joke } from './joke.model';
 import { JokeDto } from './dto/joke.dto';
 import { chuckNorrisApiUrl } from 'src/helpers/api';
+import { GetJokeFilterDto } from './dto/get-joke-filter.dto';
 
 @Injectable()
 export class JokesService {
   constructor(private httpService: HttpService) {}
 
-  async getRandomJoke(): Promise<JokeDto> {
+  async getRandomJoke(filterDto: GetJokeFilterDto): Promise<JokeDto> {
+    const { name, category } = filterDto;
+    let url: string = `${chuckNorrisApiUrl}/jokes/random?`;
+
+    if (category) {
+      url += `category=${category}`;
+    }
+
     const { data } = await firstValueFrom(
-      this.httpService.get<Joke>(`${chuckNorrisApiUrl}/jokes/random`).pipe(
+      this.httpService.get<Joke>(url).pipe(
         catchError((err: AxiosError) => {
           throw new HttpException(err.response.data, err.response.status);
         }),
@@ -21,7 +29,7 @@ export class JokesService {
 
     const jokeDto: JokeDto = {
       id: data.id,
-      value: data.value,
+      value: name ? data.value.replaceAll(/Chuck Norris/ig, name) : data.value,
     };
     return jokeDto;
   }
